47850452eb10b4db384c4bb98d6334d7e1b87550
DZSESAKQ <jahongirabdikadirov@gmail.com>
2022-06-08 17:03:24 +0300

    individ

---
+++ individual.py
@@ -0,0 +1,7 @@
+import math
+a = int(input("Enter 1st number "))
+b = int(input("Enter 2nd number "))
+x1 = (a + b)/2
+x2 = math.sqrt(a+b)
+print("Arithmetic mean: {0} . ".format(x1))
+print("Geometric mean: {0} . ".format(x1))
